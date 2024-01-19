ab6b253cf162a08d638e2a05744406eabe8304ea
shimainp <157105593+shimainp@users.noreply.github.com>
2024-01-19 18:54:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -39,18 +39,6 @@ app.get("/ace/worker-lua.js",function (req, res) {
   res.sendFile('/ace/worker-lua.js', { root: '.' })
 })
 
-const WebSocket = require('ws')
-const PORT = process.env.PORT || 3000;
-const wss = new WebSocket.Server({ port: PORT })
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    console.log(`Received message => ${message}`)
-  })
-  ws.send('Hello! Message From Server!!')
-})
-
-
-
 const port = 80
 
 app.listen(port, () => {
